cf0906f01819c740fa2f2acd5a2068edcb8151a4
Mathis Kimmel <mathiskimmel@hotmail.com>
2023-09-05 22:02:20 +0200

    functionally complete

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     generateSudoku();
+    output = document.getElementById("link-output");
+    document.getElementById("loader").addEventListener("change", (event) => {
+        if (event.target.value.length !== 90) return false;
+        loadURL(event.target.value);
+    })
+    document.getElementById("copy-link").addEventListener("click", () =>{
+        navigator.clipboard.writeText(document.getElementById("link-output").textContent).then(
+            () => {
+              /* clipboard successfully set */
+              console.log("success");
+            },
+            () => {
+              /* clipboard write failed */
+              console.log("failure");
+            },
+        );
+    });
 });
+
+let output;
+const inputs = [];
 
 function generateSudoku() {
     const sudoku = document.getElementById("sudoku");
@@ -13,7 +33,55 @@ function generateRow(sudoku) {
     const div = document.createElement("div");
     for (let ii = 0; ii < 9; ii++) {
         const input = document.createElement("input");
+        input.pattern = "\d";
+        input.type = "number";
+        input.minLength = 1;
+        input.maxLength = 1;
+        input.addEventListener("change", onChangeHandler);
+        input.addEventListener("keyup", onPressHandler);
+        inputs.push(input);
         div.appendChild(input);
     }
     sudoku.appendChild(div);
+}
+
+function onChangeHandler() {
+    const header = "sudoku://";
+    let body = "";
+    inputs.forEach(element => {
+        body += element.value !== "" ? element.value : 0;
+    });
+    output.textContent = `${header}${body}`;
+}
+
+function onPressHandler(event) {
+    let next = event.target;
+    if (event.key ===  "Tab") return true;
+    if (event.key ===  "Backspace") {
+        while (next = next.previousSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() === "input") {
+                next.focus();
+                break;
+            }
+        }
+    } else {
+        while (next = next.nextElementSibling) {
+            if (next == null)
+                break;
+            if (next.tagName.toLowerCase() === "input") {
+                next.focus();
+                break;
+            }
+        }
+    }
+}
+
+function loadURL(url) {
+    const numbers = url.substring(9);
+    for (let ii = 0; ii < inputs.length; ii++) {
+        inputs[ii].value = numbers[ii] !== "0" ? numbers[ii] : "";
+    }
+    onChangeHandler();
 }
